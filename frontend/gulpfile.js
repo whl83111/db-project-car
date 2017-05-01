32a56045cf1b04cdc3bc681e59261837854ae8b6
Tommy Lai <tommy1994111@gmail.com>
2017-05-01 23:57:15 +0800

    編輯gulpfile.js babel

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,5 +1,13 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 
 gulp.task('default', function() {
-    // add tasks
+    // Node source
+    gulp.src("es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist/js"))
+    // Browser source
+    gulp.src("es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist/js"))
 });
