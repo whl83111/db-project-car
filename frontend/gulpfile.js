b199f2c84429142aa900033ca1a4294380d6cf89
Tommy Lai <tommy1994111@gmail.com>
2017-05-02 00:29:49 +0800

    編輯 gulpfile.js pug

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
 
 gulp.task('default', function() {
     // Run ESLint
@@ -12,9 +14,21 @@ gulp.task('default', function() {
     gulp.src("es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("dist/js"));
+    gulp.src("sass/**/*.sass")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+    gulp.src("pug/**/*.pug")
+        .pipe(pug())
+        .pipe(gulp.dest('dist'))
     
     // Browser source
     gulp.src("es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("public/dist/js"));
+    gulp.src("es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist/js"));
+    gulp.src("pug/**/*.pug")
+        .pipe(pug())
+        .pipe(gulp.dest('public/dist'))
 });
