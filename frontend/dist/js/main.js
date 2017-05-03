3ff67c04c04f7e909631b992fb33275b075d4274
Tommy Lai <tommy1994111@gmail.com>
2017-05-03 19:13:40 +0800

    編輯 .babelrc

---
+++ frontend/dist/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _vue = require("vue");
+
+var _vue2 = _interopRequireDefault(_vue);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+new _vue2.default();
