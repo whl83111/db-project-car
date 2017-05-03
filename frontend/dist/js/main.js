dc17f275c83c8ba14811ac374aace52e2c2ba8f5
Tommy Lai <tommy1994111@gmail.com>
2017-05-03 19:52:46 +0800

    debug

--- frontend/dist/js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _vue = require("vue");
-
-var _vue2 = _interopRequireDefault(_vue);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-new _vue2.default();
