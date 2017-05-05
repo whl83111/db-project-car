2f0b6c4d760ab520bba660324af329c83fcee313
Tommy Lai <tommy1994111@gmail.com>
2017-05-05 14:24:03 +0800

    編輯runServer.py

--- runServer.py
+++ runServer.py
@@ -2,22 +2,37 @@ from backend import connectToMySQL
 from flask import Flask, render_template
 import os
 
-FRONTEND_PATH = os.path.join('./','frontend', 'public', 'dist')
+FRONTEND_PATH = os.path.join('frontend', 'dist')
 
-app = Flask(__name__, template_folder = FRONTEND_PATH,
-            static_folder = FRONTEND_PATH)
+app = Flask(__name__, template_folder=FRONTEND_PATH,
+            static_folder=FRONTEND_PATH)
+
+app.config.update(
+	DEBUG = True,
+	TEMPLATES_AUTO_RELOAD = True,
+	SEND_FILE_MAX_AGE_DEFAULT = 1,
+)
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
-@app.route("/api/select", methods = ['GET', 'POST'])
+
+@app.route("/api/select", methods=['GET', 'POST'])
 def select():
     if request.method == 'GET':
         return 'GET'
     elif request.method == 'POST':
+        mysql = connectToMySQL.connectMySQL(host='localhost',
+                                            user='root',
+                                            password='root',
+                                            db='car'
+                                            )
         return 'POST'
 
+@app.route("/api/insert", methods=['GET', 'POST'])
+def insert():
+    pass
+
 if __name__ == "__main__":
-    print(FRONTEND_PATH)
     app.run()
