bad9a0ea8eb49bc569b2105a801bd85a9d542ef9
Tommy Lai <tommy1994111@gmail.com>
2017-05-07 01:32:53 +0800

    frontend

--- frontend/es6/main.js
+++ frontend/es6/main.js
@@ -1,22 +1,82 @@
 Vue.options.delimiters = ['<%', '%>'];
 
+var search = new Vue({
+    el: '#search',
+    data: {
+        brands: [],
+        shifts: [],
+        years: [],
+        regions: [],
+        selected: {
+            brand: null,
+            shift: null,
+            year: null,
+            region: null,
+            price: {
+                min: null,
+                max: null
+            },
+            displacement: {
+                min: null,
+                max: null
+            }
+        }
+    },
+    beforeMount: function () {
+        this.distinct('brands');
+        this.distinct('shifts');
+        this.distinct('years');
+        this.distinct('regions');
+    },
+    beforeUpdate: function () {
+        
+    },
+    methods: {
+        distinct: function (target) {
+            let temp = this;
+            axios.post('api/distinct', {
+                targetColumn: target
+            })
+            .then(function (response) {
+                temp[target] = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        },
+        sendSearch: function () {
+            axios.post('api/select', this.selected)
+                .then(function (response) {
+                    app.cars = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        clickTest: function () {
+            console.log('Clicked.')
+        }
+    }
+});
+
 var app = new Vue({
     el: '#app',
     data: {
-        cars: [
-            {
-                id: 3,
-                brand: 'toyota',
-                name: 'Supra',
-                shift: 'manual'
-            },
-            {
-                id: 4,
-                brand: 'toyota',
-                name: 'AE86',
-                shift: 'manual'
-            }
-        ]
+        cars: []
     },
+    beforeMount: function () {
+        let temp = this;
+        axios.post('api/select', {})
+            .then(function (response) {
+                console.log(response.data);
+                temp.cars = response.data;
+            })
+            .catch(function (error) {
+                console.log(error)
+            });
+    },
+    methods:{
+        
+    }
 
 });
--- frontend/public/dist/js/main.js
+++ frontend/public/dist/js/main.js
@@ -2,20 +2,73 @@
 
 Vue.options.delimiters = ['<%', '%>'];
 
+var search = new Vue({
+    el: '#search',
+    data: {
+        brands: [],
+        shifts: [],
+        years: [],
+        regions: [],
+        selected: {
+            brand: null,
+            shift: null,
+            year: null,
+            region: null,
+            price: {
+                min: null,
+                max: null
+            },
+            displacement: {
+                min: null,
+                max: null
+            }
+        }
+    },
+    beforeMount: function beforeMount() {
+        this.distinct('brands');
+        this.distinct('shifts');
+        this.distinct('years');
+        this.distinct('regions');
+    },
+    beforeUpdate: function beforeUpdate() {},
+    methods: {
+        distinct: function distinct(target) {
+            var temp = this;
+            axios.post('api/distinct', {
+                targetColumn: target
+            }).then(function (response) {
+                temp[target] = response.data;
+            }).catch(function (error) {
+                console.log(error);
+            });
+        },
+        sendSearch: function sendSearch() {
+            axios.post('api/select', this.selected).then(function (response) {
+                app.cars = response.data;
+            }).catch(function (error) {
+                console.log(error);
+            });
+        },
+        clickTest: function clickTest() {
+            console.log('Clicked.');
+        }
+    }
+});
+
 var app = new Vue({
     el: '#app',
     data: {
-        cars: [{
-            id: 3,
-            brand: 'toyota',
-            name: 'Supra',
-            shift: 'manual'
-        }, {
-            id: 4,
-            brand: 'toyota',
-            name: 'AE86',
-            shift: 'manual'
-        }]
-    }
+        cars: []
+    },
+    beforeMount: function beforeMount() {
+        var temp = this;
+        axios.post('api/select', {}).then(function (response) {
+            console.log(response.data);
+            temp.cars = response.data;
+        }).catch(function (error) {
+            console.log(error);
+        });
+    },
+    methods: {}
 
 });
